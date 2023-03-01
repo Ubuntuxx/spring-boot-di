44696c0655b1ae35c8fec9f965dc488a6db53ee9
Jose Luis <unixluisx@protonmail.ch>
2023-03-01 01:37:39 -0600

    Thymeleaf, modelo usuario, directiva if en thymeleaf

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,15 +1,29 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@Reques
+@RequestMapping("/model")
 public class IndexController {
     @GetMapping({"/", "/index", "/main"})
     public String index(Model model) {
         model.addAttribute("titulo", "Hola springboot");
         return "index";
+    }
+
+    @RequestMapping(value = "/detalle", method = RequestMethod.GET)
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setApellido("Amador");
+        usuario.setNombre("Luis");
+        usuario.setEmail("luis@mail.com");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+        return "perfil";
     }
 }
