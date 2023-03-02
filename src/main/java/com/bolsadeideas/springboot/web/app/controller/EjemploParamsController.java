f123649afb5f5bf2dd3d7c92db5c2c2f89c79ac5
Jose Luis <unixluisx@protonmail.ch>
2023-03-02 00:18:18 -0600

    Parametros del request

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -2,11 +2,16 @@ package com.bolsadeideas.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/params")
 public class EjemploParamsController {
-    public String EjemploParamsController(Model model){
-
+    @GetMapping("/string")
+    public String EjemploParamsController(@RequestParam String texto, Model model) {
+        model.addAttribute("resultado", "El parametro enviado es: " + texto);
         return "params/ver";
     }
 }
