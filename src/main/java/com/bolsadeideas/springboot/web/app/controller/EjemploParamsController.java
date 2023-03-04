10da80aa8aa5a57446491d0c73398888e41bde5a
Jose Luis <unixluisx@protonmail.ch>
2023-03-03 19:42:53 -0600

    @PathVaariable y @RequestParam

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
+
+    @GetMapping("/")
+    public String index() {
+        return "params/index";
+    }
+
     @GetMapping("/string")
     public String EjemploParamsController(@RequestParam(name = "texto", required = false) String texto, Model model) {
         model.addAttribute("resultado", "El parametro enviado es: " + texto);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String EjemploParamsController(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+        model.addAttribute("resultado", "El saludo es: " + saludo + " y el numero es: " + numero);
         return "params/ver";
     }
 }
