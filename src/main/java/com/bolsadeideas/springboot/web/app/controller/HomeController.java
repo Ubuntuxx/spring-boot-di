4bf8eb86653b6bc0b6117f744387b8d03f26dadd
Jose Luis <unixluisx@protonmail.ch>
2023-03-09 23:10:22 -0600

    Cambios!

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/HomeController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index(Model model){
-        return "redirect:https://www.google.com";
+        return "redirect:/model/index";
     }
 }
